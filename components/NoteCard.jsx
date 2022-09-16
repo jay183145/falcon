32ee8afa3945e2af57b36bda0dcaf684ca026071
jay183145 <jay183145@gmail.com>
2022-09-16 21:58:41 +0800

    card

--- components/NoteCard.jsx
+++ components/NoteCard.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { styled } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid'
+
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-
-import { useEffect, useState } from 'react';
 
 const NoteCard = () => {
   const [notes, setNotes] = useState([])
@@ -17,18 +14,23 @@ const NoteCard = () => {
       .then(res => res.json())
       .then(data => {
         console.log(data)
-        return setNotes(data.slice(0,100))
+        return setNotes(data.slice(0,10))
       })
   }, [])
   
   return (
-    <div className='ml-20 text-lg'>
-      {notes.map(note => (
-        <>
-          <p key= {note.id}>{ note.標題 }</p>
-          <p key= {note.id}>{ note.startdate }</p>
-        </>
-      ))}
+    <div className='mx-20 mb-10 text-lg'>
+      <Grid container spacing={3}>
+        {notes.map(note => (
+          <>
+            <Grid item key= {note.id} lg={3}>
+              { note.標題 }
+            </Grid>
+            <p key= {note.id}>{ note.startdate }</p>
+          </>
+        ))}
+      </Grid>
+      
     </div>
   )
 }
